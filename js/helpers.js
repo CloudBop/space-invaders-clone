29f6ab9368b4d5337d8769e82bc60982619c92ef
Cloudbop <boppincolin@gmail.com>
2020-04-11 17:02:59 +0100

    added ship+alien movement including max/min bounds, fixed helper handler error

--- js/helpers.js
+++ js/helpers.js
@@ -6,12 +6,17 @@ function Screen(width, height) {
   this.canvas = document.createElement('canvas');
   // interesting mutli assignment !!!
   this.canvas.width = this.width = width;
-  this.canvas.width = this.width = width;
+  this.canvas.height = this.height = height;
   // set ctx API
   this.ctx = this.canvas.getContext('2d');
   //
   document.body.appendChild(this.canvas);
 }
+
+Screen.prototype.clear = function() {
+  this.ctx.clearRect(0, 0, this.width, this.height);
+};
+
 Screen.prototype.drawSprite = function(sp, x, y) {
   this.ctx.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
 };
@@ -31,30 +36,22 @@ function InputHandler() {
   this.pressed = {};
   //
   var _this = this;
-  document.addEventListener(
-    'keydown',
-    function(evt) {
-      _this.down[evt.keyCode] = true;
-    },
-    false
-  );
+  document.addEventListener('keydown', function(evt) {
+    _this.down[evt.keyCode] = true;
+  });
   //
-  document.addEventListener(
-    'keyup',
-    function(evt) {
-      delete _this.pressed[evt.keyCode];
-      delete _this.pressed[evt.keyCode];
-    },
-    false
-  );
+  document.addEventListener('keyup', function(evt) {
+    delete _this.down[evt.keyCode];
+    delete _this.pressed[evt.keyCode];
+  });
   //
 }
 
-InputHandler.prototype.isDown = function(keyCode) {
-  return this.down[keyCode];
+InputHandler.prototype.isDown = function(code) {
+  return this.down[code];
 };
 
-InputHandler.prototype.isPressed = function(keyCode) {
+InputHandler.prototype.isPressed = function(code) {
   if (this.pressed[code]) {
     return false;
   } else if (this.down[code]) {
