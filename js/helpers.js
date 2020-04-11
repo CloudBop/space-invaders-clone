89bd600c4e0693770c64e8f37d4849b7fc8b7632
Cloudbop <boppincolin@gmail.com>
2020-04-11 22:12:00 +0100

    added AABBIntersect fn

--- js/helpers.js
+++ js/helpers.js
@@ -1,6 +1,9 @@
 console.log('helpers.js');
 //
 //
+function AABBIntersect(ax, ay, aw, ah, bx, by, bw, bh) {
+  return ax < bx + bw && bx < ax + aw && ay < by + bh && by < ay + ah;
+}
 //
 //
 function Bullet(x, y, vely, w, h, color) {
